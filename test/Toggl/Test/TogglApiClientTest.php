fbc6778f0e23ff92c0c5f728e759c905fc1b59e1
Jonathan Webb <jonathan.webb@acquia.com>
2013-12-11 14:56:43 -0500

    Test stub

---
+++ test/Toggl/Test/TogglApiClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Toggl\Test;
+
+class CloudApiClientTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return \Toggl\TogglApiClient
+     */
+    public function getTogglApiClient()
+    {}
+
+}
